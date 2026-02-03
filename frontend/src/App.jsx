28a87cc3a3b7dbcab9c88caa559a28528c672e92
Aleksandar Duncevic <aleksandarduncevic@gmail.com>
2026-02-03 17:08:22 +0100

    feat: add basic routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div>
       <h1>TradIQ</h1>
-    </>
-  )
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
