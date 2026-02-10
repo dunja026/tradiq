a8bb29c35cac71b2376401c413e560b27bd2b1f2
Aleksandar Duncevic <aleksandarduncevic@gmail.com>
2026-02-10 13:54:15 +0100

    chore: initialize Spring Boot backend and consolidate gitignore

---
+++ backend/src/main/java/com/tradiq/backend/TradiqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tradiq.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradiqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradiqBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tradiq/backend/TradiqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradiq.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradiqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
